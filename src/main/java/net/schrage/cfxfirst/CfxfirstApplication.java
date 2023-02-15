074614730248e20b1ab1e4f5862ed4f25d91f7d6
schragman <schragman@gmail.com>
2023-02-15 19:10:54 +0100

    First commit

---
+++ src/main/java/net/schrage/cfxfirst/CfxfirstApplication.java
@@ -0,0 +1,13 @@
+package net.schrage.cfxfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfxfirstApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CfxfirstApplication.class, args);
+  }
+
+}
---
+++ src/test/java/net/schrage/cfxfirst/CfxfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package net.schrage.cfxfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfxfirstApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
